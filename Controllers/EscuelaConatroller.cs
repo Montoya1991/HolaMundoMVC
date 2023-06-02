4efb9763473e0ea56521c8d1a36f61470d17f9b1
Toya <jdmontoya0491@hotmail.com>
2023-06-01 22:45:12 -0500

    se crean archivos desde code

---
+++ Controllers/EscuelaConatroller.cs
@@ -0,0 +1,9 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using HolaMundoMVC.Models;
+
+namespace HolaMundoMVC.Controllers;
+public class EscuelaController : Controller
+{
+    
+}
